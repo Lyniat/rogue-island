88856b873e98d8916ecafe3bc1e5b700fafd8a56
Lyniat <derlichking@gmx.net>
2016-01-20 10:54:45 +0100

    added gui

--- __init__.py
+++ __init__.py
@@ -4,6 +4,8 @@ import random
 import thread
 from ctypes import c_int
 from multiprocessing import Value
+import shelve
+import textwrap
 
 import libtcodpy as libtcod
 from src import color, island_generator, loader, nonplayercharacter, playercharacter, tiles
@@ -22,6 +24,18 @@ VISUAL_HEIGHT = 55
 VISUAL_WIDTH_OFFSET = 0
 VISUAL_HEIGHT_OFFSET = 1
 
+# sizes and coordinates relevant for the GUI
+BAR_WIDTH = 20
+BAR_WIDTH_TOP = SCREEN_WIDTH - 2
+PANEL_HEIGHT = 7
+PANEL_HEIGHT_TOP = 2
+PANEL_BOTTOM = SCREEN_HEIGHT - PANEL_HEIGHT
+PANEL_TOP = 0 + PANEL_HEIGHT_TOP
+MSG_X = BAR_WIDTH + 2
+MSG_WIDTH = SCREEN_WIDTH - BAR_WIDTH - 2
+MSG_HEIGHT = PANEL_HEIGHT - 1
+INVENTORY_WIDTH = 50
+
 LIMIT_FPS = 120  # 120 frames-per-second maximum (for testing)
 
 FOV_ALGO = 0  # default FOV algorithm
@@ -29,7 +43,15 @@ FOV_LIGHT_WALLS = True
 TORCH_RADIUS = 10
 MAX_MONSTERS = 20
 
+# GUI panel at the bottom/top of the screen
+bottom_panel = libtcod.console_new(SCREEN_WIDTH, PANEL_HEIGHT)
+top_panel = libtcod.console_new(SCREEN_WIDTH, PANEL_HEIGHT_TOP)
+numKlicks = 0
+time = 1
+
 game_status = 1  # 0 = generator, 1 = game, 2 = menu
+
+game_msgs = []
 
 shared_percent = Value(c_int)
 
@@ -248,11 +270,306 @@ def render_all():
     # blit the contents of "con" to the root console
     libtcod.console_blit(con, 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, 0, 0, 0)
 
+    # prepare to render the GUI panel
+    libtcod.console_set_default_background(top_panel, libtcod.black)
+    libtcod.console_clear(top_panel)
+    libtcod.console_set_default_background(bottom_panel, libtcod.black)
+    libtcod.console_clear(bottom_panel)
+
+    # print the game messages, one line at a time
+    y = 1
+    for (line, color_msg) in game_msgs:
+        libtcod.console_set_default_foreground(bottom_panel, color_msg)
+        libtcod.console_print_ex(bottom_panel, MSG_X, y, libtcod.BKGND_NONE, libtcod.LEFT, line)
+        y += 1
+
+    # show the player's stats
+    render_bar(bottom_panel, 1, 1, BAR_WIDTH, 'HP', player.entclass.hp, player.entclass.max_hp,
+               libtcod.light_red, libtcod.darker_red)
+    # shows the time
+    render_timeLine(top_panel, 1, 1, BAR_WIDTH_TOP, 'O', calcTime(), 24, libtcod.dark_blue)
+    #render_bar(top_panel, 1, 1, BAR_WIDTH_TOP, 'TIME', calcTime(), 24,
+     #          libtcod.light_yellow, libtcod.dark_yellow)
+    # display names of objects under the mouse
+    libtcod.console_set_default_foreground(bottom_panel, libtcod.light_gray)
+    libtcod.console_print_ex(bottom_panel, 1, 0, libtcod.BKGND_NONE, libtcod.LEFT, '')
+
+    # blit the contents of "panel" to the root console
+    libtcod.console_blit(bottom_panel, 0, 0, SCREEN_WIDTH, PANEL_HEIGHT, 0, 0, PANEL_BOTTOM)
+    libtcod.console_blit(top_panel, 0, 0, SCREEN_WIDTH, PANEL_HEIGHT_TOP, 0, 0, PANEL_TOP)
+
+
+def message(new_msg, color_msg=libtcod.white):
+    # split the message if necessary, among multiple lines
+    new_msg_lines = textwrap.wrap(new_msg, MSG_WIDTH)
+
+    for line in new_msg_lines:
+        # if the buffer is full, remove the first line to make room for the new one
+        if len(game_msgs) == MSG_HEIGHT:
+            del game_msgs[0]
+
+        # add the new line as a tuple, with the text and the color
+        game_msgs.append((line, color_msg))
+
+
+def menu(header, options, width):
+    if len(options) > 26: raise ValueError('Cannot have a menu with more than 26 options.')
+
+    # calculate total height for the header (after auto-wrap) and one line per option
+    header_height = libtcod.console_get_height_rect(con, 0, 0, width, SCREEN_HEIGHT, header)
+    if header == '':
+        header_height = 0
+    height = len(options) + header_height
+
+    # create an off-screen console that represents the menu's window
+    window = libtcod.console_new(width, height)
+
+    # print the header, with auto-wrap
+    libtcod.console_set_default_foreground(window, libtcod.white)
+    libtcod.console_print_rect_ex(window, 0, 0, width, height, libtcod.BKGND_NONE, libtcod.LEFT, header)
+
+    # print all the options
+    y = header_height
+    letter_index = ord('a')
+    for option_text in options:
+        text = '(' + chr(letter_index) + ') ' + option_text
+        libtcod.console_print_ex(window, 0, y, libtcod.BKGND_NONE, libtcod.LEFT, text)
+        y += 1
+        letter_index += 1
+
+    # blit the contents of "window" to the root console
+    x = SCREEN_WIDTH / 2 - width / 2
+    y = SCREEN_HEIGHT / 2 - height / 2
+    libtcod.console_blit(window, 0, 0, width, height, 0, x, y, 1.0, 0.7)
+
+    # present the root console to the player and wait for a key-press
+    libtcod.console_flush()
+    key = libtcod.console_wait_for_keypress(True)
+
+    if key.vk == libtcod.KEY_ENTER and key.lalt:  # (special case) Alt+Enter: toggle fullscreen
+        libtcod.console_set_fullscreen(not libtcod.console_is_fullscreen())
+
+    # convert the ASCII code to an index; if it corresponds to an option, return it
+    index = key.c - ord('a')
+    if index >= 0 and index < len(options): return index
+    return None
+
+"""
+def get_names_under_mouse():
+    global mouse
+
+    # return a string with the names of all objects under the mouse
+    (x, y) = (mouse.cx, mouse.cy)
+
+    # create a list with the names of all objects at the mouse's coordinates and in FOV
+    names = [obj.name for obj in objects
+             if obj.x == x and obj.y == y]
+
+    names = ', '.join(names)  # join the names, separated by commas
+    return names.capitalize()
+"""
+
+def render_bar(panel, x, y, total_width, name, value, maximum, bar_color, back_color):
+    # render a bar (HP, experience, etc). first calculate the width of the bar
+    bar_width = int(float(value) / maximum * total_width)
+
+    # render the background first
+    libtcod.console_set_default_background(panel, back_color)
+    libtcod.console_rect(panel, x, y, total_width, 1, False, libtcod.BKGND_SCREEN)
+
+    # now render the bar on top
+    libtcod.console_set_default_background(panel, bar_color)
+    if bar_width > 0:
+        libtcod.console_rect(panel, x, y, bar_width, 1, False, libtcod.BKGND_SCREEN)
+
+    # finally, some centered text with the values
+    libtcod.console_set_default_foreground(panel, libtcod.white)
+    libtcod.console_print_ex(panel, x + total_width / 2, y, libtcod.BKGND_NONE, libtcod.CENTER,
+                             name + ': ' + str(value) + '/' + str(maximum))
+
+
+def calcTime():
+    global time
+    global numKlicks
+    print numKlicks
+    """if numKlicks < 12:
+        time = 1
+    elif 12 < numKlicks <= 24:
+        time = 2
+    elif 24 < numKlicks <= 36:
+        time = 3
+    elif 36 < numKlicks <= 48:
+        time = 4
+    elif 48 < numKlicks <= 60:
+        time = 5
+    elif 60 < numKlicks <= 72:
+        time = 6
+    elif 72 < numKlicks <= 84:
+        time = 7
+    elif 84 < numKlicks <= 96:
+        time = 8
+    elif 96 < numKlicks <= 108:
+        time = 9
+    elif 108 < numKlicks <= 120:
+        time = 10
+    elif 120 < numKlicks <= 132:
+        time = 11
+    elif 132 < numKlicks <= 144:
+        time = 12
+    elif 144 < numKlicks <= 156:
+        time = 13
+    elif 156 < numKlicks <= 168:
+        time = 14
+    elif 168 < numKlicks <= 180:
+        time = 15
+    elif 180 < numKlicks <= 192:
+        time = 16
+    elif 192 < numKlicks <= 204:
+        time = 17
+    elif 204 < numKlicks <= 216:
+        time = 18
+    elif 216 < numKlicks <= 228:
+        time = 19
+    elif 228 < numKlicks <= 240:
+        time = 20
+    elif 240 < numKlicks <= 252:
+        time = 21
+    elif 252 < numKlicks <= 264:
+        time = 22
+    elif 264 < numKlicks <= 276:
+        time = 23
+    elif 276 < numKlicks <= 288:
+        time = 24
+    elif numKlicks > 288:
+        time = 0
+        numKlicks = 0"""
+    time = numKlicks
+    if numKlicks > BAR_WIDTH_TOP:
+        time = 1
+        numKlicks = 0
+
+    return time
+
+
+def render_timeLine(panel, x, y, total_width, name, value, maximum, back_color):
+     # render a bar (HP, experience, etc). first calculate the width of the bar
+    bar_width = int(float(value) / maximum * total_width)
+
+    # render the background first
+    libtcod.console_set_default_background(panel, back_color)
+    libtcod.console_rect(panel, x, y, total_width, 1, False, libtcod.BKGND_SCREEN)
+
+    # now render the bar on top
+    libtcod.console_set_default_background(panel, libtcod.blue)
+    x = value
+    if bar_width > 0:
+       libtcod.console_put_char_ex(panel, x, y, 15, libtcod.yellow, libtcod.blue)
+
+def perk_menu(header, options1, options2):
+    # show a menu with each item of the inventory as an option
+    # if len(inventory) == 0:
+    #   options = ['Inventory is empty.']
+    # else:
+    #    options = [item.name for item in inventory]
+    options = []
+    if options1:
+        options = ['Opt 1', 'Opt 2', 'Opt 3']
+    elif options2:
+        options = ['Opt 1', 'Opt 2', 'Opt 3', 'Opt 4', 'Opt 5', 'Opt 6', 'Opt 7', 'Opt 8', 'Opt 9', 'Opt 10']
+
+    index = menu(header, options, INVENTORY_WIDTH)
+
+    # if an item was chosen, return it
+    # if index is None or len(inventory) == 0: return None
+    # return inventory[index].item
+    return index
+
+
+def msgbox(text, width=50):
+    menu(text, [], width)  # use menu() as a sort of "message box"
+
 
 def game_over(player):
     print 'You died!'
     player.char = '%'
     player.color = libtcod.dark_red
+
+def save_game():
+    # open a new empty shelve (possibly overwriting an old one) to write the game data
+    file = shelve.open('savegame', 'n')
+    file['map'] = map
+    file['objects'] = objects
+    file['player_index'] = objects.index(player)  # index of player in objects list
+    file['inventory'] = inventory
+    file['game_msgs'] = game_msgs
+    file['game_state'] = game_state
+    file.close()
+
+"""
+def play_game():
+    global key, mouse
+
+    player_action = None
+
+    mouse = libtcod.Mouse()
+    key = libtcod.Key()
+    while not libtcod.console_is_window_closed():
+        # render the screen
+        libtcod.sys_check_for_event(libtcod.EVENT_KEY_PRESS | libtcod.EVENT_MOUSE, key, mouse)
+        render_all()
+
+        libtcod.console_flush()
+
+        # erase all objects at their old locations, before they move
+        for object in objects:
+            object.clear()
+
+        # handle keys and exit game if needed
+        player_action = handle_keys()
+        if player_action == 'exit':
+            save_game()
+            break
+
+        # let monsters take their turn
+        if game_state == 'playing' and player_action != 'didnt-take-turn':
+            for object in objects:
+                if object.ai:
+                    object.ai.take_turn()
+"""
+
+def load_game():
+    # open the previously saved shelve and load the game data
+    global map, objects, player, inventory, game_msgs, game_state
+
+    file = shelve.open('savegame', 'r')
+    map = file['map']
+    objects = file['objects']
+    player = objects[file['player_index']]  # get index of player in objects list and access it
+    inventory = file['inventory']
+    game_msgs = file['game_msgs']
+    game_state = file['game_state']
+    file.close()
+
+"""
+def new_game():
+    global player, inventory, game_msgs, game_state
+
+    # create object representing the player
+    playerentity = playercharacter.entity(hp=100, strength=5, agility=5, intelligence=5, vitality=5, on_death=game_over)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, '@', 'player', libtcod.white, blocks=True,
+                    entclass=playerentity)
+
+    # generate map (at this point it's not drawn to the screen)
+
+    game_state = 'playing'
+    inventory = []
+
+    # create the list of game messages and their colors, starts empty
+    game_msgs = []
+
+    # welcoming message
+    message('Welcome to Rogue Island stranger!', libtcod.red)
+"""
 
 
 def monster_death(monster):
@@ -266,6 +583,7 @@ def monster_death(monster):
 
 
 def handle_keys():
+    global numKlicks
     # key = libtcod.console_check_for_keypress()  #real-time
     key = libtcod.console_wait_for_keypress(True)  # turn-based
 
@@ -276,21 +594,37 @@ def handle_keys():
     elif key.vk == libtcod.KEY_ESCAPE:
         return True  # exit game
 
-    # movement keys
     if libtcod.console_is_key_pressed(libtcod.KEY_UP):
         attackmove(0, -1)
-
+        numKlicks += 1
     elif libtcod.console_is_key_pressed(libtcod.KEY_DOWN):
         attackmove(0, 1)
-
+        numKlicks += 1
     elif libtcod.console_is_key_pressed(libtcod.KEY_LEFT):
         attackmove(-1, 0)
-
+        numKlicks += 1
     elif libtcod.console_is_key_pressed(libtcod.KEY_RIGHT):
         attackmove(1, 0)
-
+        numKlicks += 1
     else:
-        return 'player_pass'
+        # return 'player_pass'
+        key_char = chr(key.c)
+
+        if key_char == 'p' and not libtcod.console_is_key_pressed(key):
+            # show in-game menu
+            chosen_option = perk_menu('Press the key next to the perk you want to use.\n', 1, 0)
+            if chosen_option is 0:
+                chosen_perk = perk_menu('Perk Menu with 10 Options.\n', 0, 1)
+
+            elif chosen_option is 1:
+                chosen_perk = perk_menu('Perk Menu with 10 Options.\n', 0, 1)
+
+            elif chosen_option is 2:
+                chosen_perk = perk_menu('Perk Menu with 10 Options.\n', 0, 1)
+
+
+
+
 
 
 def load_tiles():
